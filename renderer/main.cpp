8eac8bb67adc7959109e694d8862a3fff9eceb8e
dr-programmer <drtopuzov@gmail.com>
2024-09-09 19:10:09 +0300

    current

--- renderer/main.cpp
+++ renderer/main.cpp
@@ -34,8 +34,6 @@ S
     }
 
     vector *normal = vector_create_init(complex_create(1/sqrt(2), 0), complex_create(0, 1/sqrt(2)));
-    quantum_gate *o = quantum_gate_create(2);
-    o->fields[1][1] = complex_create(0, 1);
 
     std::ofstream ppmFileStream("test.ppm", 
                     std::ios::out | std::ios::binary);
@@ -77,21 +75,10 @@ S
             vector *x_check = matrix_mul(quantum_gate_create_hadamard(), qm);
 
             bool isIn = true;
-            for(unsigned int i = 0; i < 3; i++) {
-                unsigned int tail = i;
-                unsigned int head = (i + 1) % 3;
-                S
-                vector *arm = matrix_sub(points[head], points[tail]);
-                vector *dir = matrix_sub(qm, points[tail]);
-                vector *temp = matrix_mul(o, arm);
-                struct complex check = vector_inner_product(normal, temp);
-                if(check.real < 0) {
-                    isIn = false;
-                    E
-                    break;
-                }
-                E
-            }
+            if(pow(qm->fields[0][0].real, 2) - pow(qm->fields[1][0].real, 2) 
+                    < points[0]->fields[0][0].real 
+                && pow(x_check->fields[0][0].real, 2) - pow(x_check->fields[1][0].real, 2) 
+                    < points[0]->fields[0][0].real) isIn = false;
 
             int x_color = 0;
             int z_color = 0;
