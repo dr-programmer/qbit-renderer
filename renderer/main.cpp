6f0ff241de4c5cd64f7f96985879da69bf9dc8b6
dr-programmer <drtopuzov@gmail.com>
2024-09-10 19:05:02 +0300

    Successfully rendered a rectangle

--- renderer/main.cpp
+++ renderer/main.cpp
@@ -14,23 +14,70 @@ static const int imageWidth = 1920;
 static const int imageHeight = 1080;
 static const int maxColorComponent = 255;
 
+struct matrix *convert_2dc_3dr(vector *v) {
+    if(!v) return NULL;
+
+    float y = pow(complex_get_absolute(v->fields[0][0]), 2) 
+                    - pow(complex_get_absolute(v->fields[1][0]), 2);
+    float x_sign = complex_get_absolute(v->fields[1][0]) != 0 
+                    ? (v->fields[1][0].real 
+                        / complex_get_absolute(v->fields[1][0])) 
+                    : 0;
+    float sin_phi = (pow(complex_get_absolute(v->fields[0][0]), 2) 
+                        + pow(complex_get_absolute(v->fields[1][0]), 2)) 
+                    * x_sign;
+    float x = sqrt(1 - pow(y, 2)) * sin_phi;
+    float z_sign = complex_get_absolute(v->fields[1][0]) != 0 
+                    ? (v->fields[1][0].imaginary 
+                        / complex_get_absolute(v->fields[1][0])) 
+                    : 0;
+    float z = 1 - pow(sin_phi, 2) == 1 
+                ? sqrt(1 - pow(y, 2)) * sqrt(1 - pow(sin_phi, 2)) * z_sign 
+                : 0;
+
+    // float x, y, z;
+    // S
+    // quantum_operator *d = matrix_mul(v, vector_get_dual(v));
+    // x = -2 * d->fields[0][0].real;
+    // z = 2 * d->fields[1][0].imaginary;
+    // y = complex_mul(complex_create(2, 0), d->fields[0][0]).real - 1;
+    // E
+
+    struct matrix *result = matrix_create_empty(3, 1);
+
+    result->fields[0][0].real = x;
+    result->fields[1][0].real = y;
+    result->fields[2][0].real = z;
+
+    return result;
+}
+
 int main() {
 
 smart_allocation_global_turn_off = 1;
 
 S
     S
+    struct complex c_0 = complex_create(0, 0);
+    struct complex c_1 = complex_create(1, 0);
     qubit *points[3] = {
-        qubit_create_init(complex_create(1, 0), complex_create(0, 0)), 
-        qubit_create_init(complex_create(0, 0), complex_create(0, 0)), 
-        matrix_mul(quantum_gate_create_hadamard(), qubit_create())
+        qubit_create_init(c_1, c_0), 
+        qubit_create_init(complex_create(1/sqrt(2), 0), complex_create(0, -1/sqrt(2))), 
+        matrix_mul(quantum_gate_create_hadamard(), qubit_create_init(c_1, c_0))
     };
     for(unsigned int i = 0; i < 3; i++) P(points[i]);
     E
 
     for(unsigned int i = 0; i < 3; i++) {
-        matrix_print(points[i]);
         printf("\n");
+        matrix_print(points[i]);
+
+        struct matrix *coords_3d = convert_2dc_3dr(points[i]);
+
+        printf("x: %f, y: %f, z: %f \n", 
+                    coords_3d->fields[0][0].real, 
+                    coords_3d->fields[1][0].real, 
+                    coords_3d->fields[2][0].real);
     }
 
     vector *normal = vector_create_init(complex_create(1/sqrt(2), 0), complex_create(0, 1/sqrt(2)));
@@ -52,17 +99,19 @@ S
             S
             qubit *qm = NULL;
             // |p> = cos(th/2)|0> + sin(th/2)e^(i*gam)|1>
+            float theta = acos(y);
+            float phi = atan2(0, x);
             qm = matrix_add(
-                matrix_mul_scalar(complex_create(x, 0), qubit_create()), 
+                matrix_mul_scalar(complex_create(cos(theta/2), 0), qubit_create()), 
                 matrix_mul_scalar(
-                    complex_create(y, 0), 
+                    complex_mul(complex_create(sin(theta/2), 0), complex_create(cos(phi), sin(phi))), 
                     qubit_create_init(complex_create(0, 0), complex_create(0, 1))
                 )
             );
 
-            float length = vector_inner_product(qm, qm).real;
-            qm->fields[0][0].real /= length;
-            qm->fields[1][0].real /= length;
+            // float length = vector_inner_product(qm, qm).real;
+            // qm->fields[0][0].real /= length;
+            // qm->fields[1][0].real /= length;
 
             float scale_factor = fabs(
                 vector_inner_product(points[0], normal).real 
@@ -71,6 +120,7 @@ S
 
             qm->fields[0][0].real *= scale_factor;
             qm->fields[1][0].real *= scale_factor;
+            qm->fields[1][0].imaginary *= scale_factor;
 
             vector *x_check = matrix_mul(quantum_gate_create_hadamard(), qm);
 
